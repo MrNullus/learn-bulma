7af32b28d6b45ad7bb6960bc4a64a88bd2ccbeaa
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-01-14 20:03:32 -0300

    changes

--- module-1/index.js
+++ module-1/index.js
@@ -5,3 +5,25 @@ const navbarMenu =  document.querySelector("#nav-links");
 burgerIcon.addEventListener("click", () => {
 	navbarMenu.classList.toggle("is-active");
 });
+
+
+// tabs
+const tabs = document.querySelectorAll('.tabs li');
+const tabContent = document.querySelectorAll('#tab-content > div');
+
+tabs.foreach(( tab ) => {
+	tab.addEventListener('click', () => {
+		tabs.foreach(( item ) => item.classList.remove('is-active'));
+		tab.classList.add('is-active');
+
+		const target = tab.dataset.target;
+		console.log(target);
+
+		tabContent.foreach(( box ) => {
+			if (box.getAttribute('id') === target) {
+				box.classList.remove('is-hidden');
+			}
+			box.classList.add('is-hidden');
+		});
+	});
+});
